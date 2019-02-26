525b3d580fc723c6227daa5d0a4e1a0534736854
David Ibarzabal <david_soc@hotmail.com>
2019-02-26 11:33:48 -0500

    mwthod lancer marche

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,11 +5,16 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Controller {
     //code inspiration
@@ -20,27 +25,35 @@ public class Controller {
     public Circle ball;
     @FXML
     public void lancer(){
-
-        int[] counter={0};
-        double vitesseIni=50;
+        double vitesse =150;
         double angle=20;
-        double vitesseX=vitesseIni*Math.cos(angle);
-        double vitesseYIni=vitesseIni*Math.sin(angle);
+        int[] counter={0};
+        angle=angle*0.0174533;
+        double vitesseX=vitesse*(double)(Math.cos(angle));
+        double vitesseYIni=vitesse*(double)(Math.sin(angle));
         double gravite=-9.8;
+
+        SimpleDoubleProperty stepX=new SimpleDoubleProperty(0);
+        SimpleDoubleProperty stepY=new SimpleDoubleProperty(0);
+        SimpleDoubleProperty posX=new SimpleDoubleProperty(0);
+        SimpleDoubleProperty posY=new SimpleDoubleProperty(0);
+        SimpleDoubleProperty lastPosX=new SimpleDoubleProperty(0);
+        SimpleDoubleProperty lastPosY=new SimpleDoubleProperty(0);
+
         final Timeline timeline = new Timeline();
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.setAutoReverse(false);
-        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(50),event -> {
-            double lastX=0;
-            double lastY=0;
-            double y = (vitesseYIni*(counter[0]*0.01))+((gravite*(counter[0]*0.01)*(counter[0]*0.01))/2);
-            double x = (vitesseX*(counter[0]*0.01));
-            ball.setTranslateX(ball.getCenterX()+(x-lastX));
-            ball.setCenterX(ball.getCenterX()+(x-lastX));
-            ball.setTranslateY(ball.getCenterY()-(y-lastY));
-            ball.setCenterY(ball.getCenterY()-(y-lastY));
-            lastX=x;
-            lastY=y;
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(1),event -> {
+            posX.set(vitesseX*(counter[0]*0.004));
+            posY.set((vitesseYIni*(counter[0]*0.004))+((gravite*(counter[0]*0.004)*(counter[0]*0.004))/2));
+            stepX.set((posX.get()- lastPosX.get()));
+            stepY.set(posY.get()-lastPosY.get());
+            lastPosX.set(posX.get());
+            lastPosY.set(posY.get());
+            ball.setTranslateX(ball.getCenterX()+(stepX.get()));
+            ball.setCenterX(ball.getCenterX()+(stepX.get()));
+            ball.setTranslateY(ball.getCenterY()-(stepY.get()));
+            ball.setCenterY(ball.getCenterY()-(stepY.get()));
             counter[0]++;
 
         }));
